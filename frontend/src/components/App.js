be69be6ee85c17845cabba8328104e9a11a60c14
tetiana-zagoruiko <t.zagoruiko@gmail.com>
2021-01-24 01:41:37 -0800

    jan 24

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -138,7 +138,7 @@ function App() {
   function handleRegister(password, email) {
     auth.register(password, email)
       .then((res) => {
-        if (!res || res.statusCode === 400 || res.statusCode === 500 || res.message === "An error occurred on the server") {
+        if (!res || res.statusCode === 400 || res.statusCode === 500 || res.message === "An error occurred on the server" || res.statusCode === 409 ) {
           openingInfoTooltip(false);
         } else {
           openingInfoTooltip(true);
